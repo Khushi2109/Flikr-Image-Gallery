cc357137ff0f9e5f10dfaa2625cd71ff96771e36
Khushi2109 <khushikri2109@gmail.com>
2021-06-30 23:15:47 +0530

    Initial commit

---
+++ app/src/main/java/dev/khushi/imagegallery/view/SearchRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package dev.khushi.imagegallery.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.khushi.imagegallery.R
+import dev.khushi.imagegallery.model.FlickrPhoto
+import dev.khushi.imagegallery.util.getProgressDrawable
+import dev.khushi.imagegallery.util.loadImage
+import kotlinx.android.synthetic.main.layout_grid_item.view.*
+
+class SearchRecyclerViewAdapter(var listFlickrPhoto: ArrayList<FlickrPhoto>) :
+    RecyclerView.Adapter<SearchRecyclerViewAdapter.ViewHolder>() {
+
+    fun updatePhoto(newPhoto: List<FlickrPhoto>) {
+        listFlickrPhoto.clear()
+        listFlickrPhoto.addAll(newPhoto)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.layout_grid_item, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listFlickrPhoto[position])
+    }
+
+    override fun getItemCount() = listFlickrPhoto.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val imageView = view.image
+
+        private val progressDrawable = getProgressDrawable(view.context)
+
+        fun bind(flickrPhoto: FlickrPhoto) {
+            imageView.loadImage(flickrPhoto.url, progressDrawable)
+        }
+    }
+}
